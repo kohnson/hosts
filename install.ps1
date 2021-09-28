3312e69fea128c87129797f57fa0e012194f02e2
kohnson <scp100j@gmail.com>
2021-09-28 15:59:09 -0400

    Removed part of the script that throws an error despite there being none

--- install.ps1
+++ install.ps1
@@ -1,12 +1,10 @@
 ## Porn blocker installation script
 
 If ([Security.Principal.WindowsIdentity]::GetCurrent().Groups -contains 'S-1-5-32-544') {
-	If (Get-Content ".\hosts.db" >> "C:\Windows\System32\drivers\etc\hosts") {
-		Write-Host -ForegroundColor Green "Success. Your PC will now refuse to connect to porn sites."
-		Write-Host -ForegroundColor Yellow "A reboot may be required."
-	}
-	Else {Write-Host -ForegroundColor Red "An error occured."}
+	Get-Content ".\hosts.db" >> "C:\Windows\System32\drivers\etc\hosts" # Append hosts.db to hosts
+	Write-Host -ForegroundColor Green "Success. Your PC will now refuse to connect to porn sites."
+	Write-Host -ForegroundColor Yellow "A reboot may be required."
 }
-Else {	Write-Host -ForegroundColor Red "You need to run PowerShell as administrator for this script to work."}
+Else {Write-Host -ForegroundColor Red "You need to run PowerShell as administrator for this script to work."} # Exit if script is not run as administrator
 
 Pause
